d925a3f4e1bc1f69174faf2ebfda8cfbac44d199
Fernando <fgilcorrea08@correo.unicordoba.edu.co>
2025-06-15 20:13:07 -0500

    refactor: renombrar parámetros en la función scrollBehavior para mayor claridad

--- src/router/index.ts
+++ src/router/index.ts
@@ -114,7 +114,7 @@ const routes: Array<RouteRecordRaw> = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
-  scrollBehavior(to, from, savedPosition) {
+  scrollBehavior(_to, _from, savedPosition) {
     if (savedPosition) {
       return savedPosition;
     } else {
